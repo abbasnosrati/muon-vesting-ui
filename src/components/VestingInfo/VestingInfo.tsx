445953ec0f02888c2a16f62def34f517e7b495d7
abbasnosrati <abbas.nosrati68@gmail.com>
2025-05-05 15:31:11 +0330

    remove dead code

--- src/components/VestingInfo/VestingInfo.tsx
+++ src/components/VestingInfo/VestingInfo.tsx
@@ -11,14 +11,14 @@ const VestingInfo = () => {
           Stats
         </div>
         <div className="muon actions-content  w-full px-4 mt-4 py-8 min-h-[400px] md:min-h-[428px] md:max-h-[424px] overflow-hidden flex flex-col">
-          <div className=" border-t border-b m-4 py-6">
+          <div className=" border-t  m-4 py-6">
             <div className="w-full flex items-center justify-between">
               <div className="border-b w-full max-w-[200px] pb-6">
                 Vesting Start Date:
               </div>
               <div className="border-b w-full flex justify-center items-center max-w-[150px] pb-4">
                 <mark className="bg-textBackGround p-1">
-                  {vestingInfo?.startDate}
+                  {vestingInfo?.startDate ?? ""}
                 </mark>
               </div>
             </div>
@@ -29,42 +29,20 @@ const VestingInfo = () => {
               </div>
               <div className="border-b w-full flex justify-center max-w-[150px] py-5">
                 <mark className="bg-textBackGround p-1">
-                  {vestingInfo?.endDate}
+                  {vestingInfo?.endDate ?? ""}
                 </mark>
               </div>
             </div>
 
-            <div className="w-full flex items-center justify-between">
-              <div className="w-full max-w-[200px] pt-6">Duration:</div>
-              <div className=" w-full flex justify-center max-w-[150px] pt-5">
+            <div className="w-full flex items-center justify-between border-b">
+              <div className="w-full max-w-[200px] ">Duration:</div>
+              <div className=" w-full flex justify-center max-w-[150px] py-4">
                 <mark className="bg-textBackGround p-1">
-                  {vestingInfo?.duration}
+                  {vestingInfo?.duration ?? ""}
                 </mark>
               </div>
             </div>
           </div>
-          {/* <div className="mt-5 text-[12px] leading-5">
-            <div className="border-t text-base  flex justify-between gap-4">
-              <div className="border-b py-4 w-full text-nowrap">
-                Vesting Start Date:
-              </div>
-              <div className="border-b py-4 w-full flex justify-center">
-                <mark className="bg-textBackGround p-1">2025/04/30</mark>
-              </div>
-            </div>
-            <div className="flex justify-between text-base gap-4">
-              <div className="border-b py-4 w-full">Vesting End Date:</div>
-              <div className="border-b py-4 w-full flex justify-center bg-yellow-300">
-                <mark className="bg-textBackGround p-1">2025/04/30</mark>
-              </div>
-            </div>
-            <div className="flex justify-between text-base border-b">
-              <div className=" py-4 w-full">Duration:</div>
-              <div className=" py-4 w-full flex justify-center bg-red-400">
-                <mark className="bg-textBackGround p-1 px-2 ">11 Months</mark>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
